9fc4a4f0d2e9a92da4bd64f3b2cd96a61915eaa1
Cameron Bernhardt <cambernhardt@me.com>
2017-07-16 17:25:09 -0400

    Add colors module

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const login = require("facebook-chat-api");
 const fs = require("fs");
 const readline = require("readline");
+const colored = require("./colors").colorString;
 let gapi, last;
 
 try {
-	login({"appState": JSON.parse(fs.readFileSync("appstate.json", "utf8"))}, callback);
-} catch(e) {
-	login({"email": "", "password": ""}, (err, api) => {
-		if(err) return console.error(err);
+	login({ "appState": JSON.parse(fs.readFileSync("appstate.json", "utf8")) }, callback);
+} catch (e) {
+	login({ "email": credentials.email, "password": credentials.password }, (err, api) => {
+		if (err) return console.error(err);
 
 		fs.writeFileSync("appstate.json", JSON.stringify(api.getAppState()));
 		callback(err, api);
@@ -15,8 +16,8 @@ try {
 }
 
 function callback(err, api) {
-	if(err) return console.error(err);
-	api.setOptions({"logLevel": "warn"});
+	if (err) return console.error(err);
+	api.setOptions({ "logLevel": "warn" });
 	gapi = api;
 
 	const rl = readline.createInterface({
@@ -24,11 +25,11 @@ function callback(err, api) {
 		"output": process.stdout
 	});
 	rl.setPrompt("> ");
-	
+
 	api.listen((err, msg) => {
 		api.getThreadInfo(msg.threadID, (err, tinfo) => {
 			api.getUserInfo(msg.senderID, (err, uinfo) => {
-				console.log(`\x1b[36m${uinfo[msg.senderID].firstName}\x1b[0m in \x1b[32m${tinfo.name}\x1b[0m: ${msg.body}`);
+				console.log(`${colored(uinfo[msg.senderID].firstName, "fgblue")} in ${colored(tinfo.name, "fggreen")} ${msg.body}`);
 			});
 		});
 	});
@@ -38,8 +39,8 @@ function callback(err, api) {
 		const terminator = line.indexOf(":");
 		if (terminator == -1) {
 			if (last) {
-				api.sendMessage(line, last, (err) => {
-					if(!err) { console.log("\x1b[42m(sent)\x1b[0m"); } else { console.log("\x1b[41m(not sent)\x1b[0m"); }
+				api.sendMessage(line, last.threadId, (err) => {
+					if (!err) { console.log(colored("sent", "bggreen")); } else { console.log(colored("(not sent)", "bgred")); }
 				});
 			} else {
 				console.log("No prior chat");
@@ -48,15 +49,19 @@ function callback(err, api) {
 			const search = line.substring(0, terminator);
 			getGroup(search, (err, group) => {
 				if (!err) {
-					api.sendMessage(line.substring(terminator+1), group.threadID, (err) => {
-						if (!err) { console.log("\x1b[42m(sent)\x1b[0m"); } else { console.log("\x1b[41m(not sent)\x1b[0m"); }
+					api.sendMessage(line.substring(terminator + 1), group.threadID, (err) => {
+						if (!err) { console.log(colored("sent", "bggreen")); } else { console.log(colored("(not sent)", "bgred")); }
 					});
-					last = group.threadID;
+					last = {
+						"threadId": group.threadID,
+						"name": group.name
+					};
 				} else {
 					console.log(err);
 				}
 			});
 		}
+		rl.setPrompt(last ? colored(`[${last.name}] `, "fggreen") : "> ");
 		rl.prompt();
 	});
 }
@@ -64,10 +69,10 @@ function callback(err, api) {
 function getGroup(search, callback, api = gapi) {
 	search = search.toLowerCase();
 	api.getThreadList(0, 10, "inbox", (err, threads) => {
-		if(!err) {
-			for(let i = 0; i < threads.length; i++) {
+		if (!err) {
+			for (let i = 0; i < threads.length; i++) {
 				isCanonicalMatch(threads[i], search, (yes) => {
-					if(threads[i].name.toLowerCase().indexOf(search) > -1 || yes) {
+					if (threads[i].name.toLowerCase().indexOf(search) > -1 || yes) {
 						callback(null, threads[i]);
 						return;
 					}
@@ -80,13 +85,13 @@ function getGroup(search, callback, api = gapi) {
 }
 
 function isCanonicalMatch(thread, search, callback, api = gapi) {
-	if(thread.isCanonical) {
+	if (thread.isCanonical) {
 		const users = thread.participantIDs;
-		for(let i = 0; i < users.length; i++) {
+		for (let i = 0; i < users.length; i++) {
 			const cur = users[i];
-			if(cur != api.getCurrentUserID()) {
+			if (cur != api.getCurrentUserID()) {
 				api.getUserInfo(cur, (err, info) => {
-					if(info && info[cur].name.toLowerCase().indexOf(search) > -1){
+					if (info && info[cur].name.toLowerCase().indexOf(search) > -1) {
 						callback(true);
 					}
 				});
